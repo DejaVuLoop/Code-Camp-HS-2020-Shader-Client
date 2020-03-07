5274d8d2c8b5e29ded711ed121347a341373099f
DejaVuLoop <dejavuloop@gmail.com>
2020-03-07 14:55:16 -0700

    dont lose everything k

--- js/main.js
+++ js/main.js
@@ -2,19 +2,48 @@ let canvas;
 let sandbox;
 let editor;
 
+images = [];
+
+
+setup();
+
 function setup() {
     canvas = document.getElementById("glslCanvas");
     sandbox = new GlslCanvas(canvas);
 
     onRun();
 }
-setup();
-
-
 
 function onRun() {
-    editor = document.getElementById("editor");
+    var height = document.getElementById("heightInput").value;
+    canvas.height = height;
+    var width = document.getElementById("widthInput").value;
+    canvas.width = width;
 
+    editor = document.getElementById("editor");
     var code = editor.textContent;
+
+    counter = 0;
+    for (var image in images) {
+        console.log("adding");
+        //sandbox.setUniform("u_tex" + counter, "https://www.wikipedia.org/portal/wikipedia.org/assets/img/Wikipedia-logo-v2@1.5x.png");
+        sandbox.setUniform("u_tex"+counter, image);
+        counter++;
+    }
     sandbox.load(code);
 }
+
+function onFileSubmit(event) {
+    var reader = new FileReader();
+    var name = event.target.files[0].name;
+    reader.addEventListener("load", function() {
+        if (this.result && localStorage) {
+            images[images.length] = this.result;
+            console.log("added");
+        }
+            else {
+                alert()
+            }
+        });
+        reader.readAsDataURL(event.target.files[0]);
+    }
