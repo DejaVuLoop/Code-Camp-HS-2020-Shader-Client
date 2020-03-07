d961366e0f0c2f3072eaf072370bfc1be92c20b1
DejaVuLoop <dejavuloop@gmail.com>
2020-03-07 09:30:40 -0700

    basic setup, got glsl in and working

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function setup() {
+    var canvas = document.getElementById("glslCanvas");
+    var sandbox = new GlslCanvas(canvas);
+    
+    console.log("entered7");
+    var string_frag_code = "void main() { gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);}";
+
+    sandbox.load(string_frag_code);
+
+    sandbox.backgroundColor = "black";
+}
+
+
+setup();
