66fc027674e6a90fb048bf4f258b8432b5c8e045
DejaVuLoop <dejavuloop@gmail.com>
2020-03-07 10:38:07 -0700

    tabbing works now

--- js/editor.js
+++ js/editor.js
@@ -1,50 +1,66 @@
 let lastThemeApplicationAttempt;
 let timeBetweenApplications = 1000; // ms
 
-let dateObject = new Date() // no memory leaks pls
-
 
 function keyDown() {
+
     lastThemeApplicationAttempt = getCurrentTime();
     setTimeout(() => { attemptThemeApplication() }, 1000);
 }
 
 function getCurrentTime() {
-    return dateObject.getTime();
+    return new Date().getTime();
 
 }
 
 function attemptThemeApplication() {
     var currentTime = getCurrentTime();
-    console.log(currentTime);
+    console.log(currentTime - lastThemeApplicationAttempt);
     if (currentTime - lastThemeApplicationAttempt < timeBetweenApplications) {
         return;
     }
     else {
+        console.log("applying");
+        var editor = document.getElementById("editor");
+        var doc = editor.ownerDocument.defaultView;
+        var sel = doc.getSelection();
+        var range = sel.getRangeAt(0);
+
+
+        var tabNode = document.createTextNode("\t");
+        range.insertNode(tabNode);
+
+        range.setStartAfter(tabNode);
+        range.setEndAfter(tabNode);
+        
+
         hljs.initHighlighting.called = false;
         hljs.initHighlighting();
+
+
+        sel.addRange(range);
     }
 }
 
+$(document).keydown(function(objEvent) {
+    console.log("recognized");
+    if (objEvent.keyCode == 9) {  //tab pressed
+
+        objEvent.preventDefault(); // stops its action
 
 
+        var editor = document.getElementById("editor");
+        var doc = editor.ownerDocument.defaultView;
+        var sel = doc.getSelection();
+        var range = sel.getRangeAt(0);
 
-document.getElementById("editor").addEventListener("keypress", function(event) {
-    if (event.keyCode === 9) { // tab key
-       e.preventDefault();  // this will prevent us from tabbing out of the editor
-       console.log("got this far");
-       // now insert four non-breaking spaces for the tab key
-       var editor = document.getElementById("editor");
-       var doc = editor.ownerDocument.defaultView;
-       var sel = doc.getSelection();
-       var range = sel.getRangeAt(0);
+        var tabNode = document.createTextNode("\t");
+        range.insertNode(tabNode);
 
-       var tabNode = document.createTextNode("\u00a0\u00a0\u00a0\u00a0");
-       range.insertNode(tabNode);
+        range.setStartAfter(tabNode);
+        range.setEndAfter(tabNode);
+        sel.removeAllRanges();
+        sel.addRange(range);
 
-       range.setStartAfter(tabNode);
-       range.setEndAfter(tabNode);
-       sel.removeAllRanges();
-       sel.addRange(range);
-   }
-}, false);
+    }
+})
